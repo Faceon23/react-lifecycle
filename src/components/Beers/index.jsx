b0b81bf1fe5019ea8c4df1ed3e679314d64216f2
Roderick Fayson <rgfayson2@gmail.com>
2022-08-09 11:43:52 -0400

    more

--- src/components/Beers/index.jsx
+++ src/components/Beers/index.jsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
+import BeerCard from "./BeerCard";
+
+function Beers() {
+    const [beerList, setBeersList] = useState();
+    const [beerType,setBeerType] =useState('ale');
 useEffect(() => {
-    fetch(`http://api.sampleapis.com/beers/ale`)
+    fetch(`https://api.sampleapis.com/beers/${beerType}`)
     .then(response => response.json())
     .then(beers => setBeersList(beers))
     .catch(alert);
-},[]);
-
-function Beers() {
-    const [beerList, setBeer] = useState();
-
+    
+},  [beerType]);
     if(!beerList) {
         return <h2>Loading...</h2>
     }
     return (
+        <>
+        <button onClick={() => setBeerType('ale')}>Ale</button>
+        <button onClick={() => setBeerType('stouts')}>Stouts</button>
+        <section id="beerList">
+            {beerList.map(beer => (
+            <BeerCard key={beer.id} beer={beer} />
+            ))}
 
-
+        </section>  
+        </>
     )
 }
 
